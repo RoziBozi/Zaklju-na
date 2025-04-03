6c33b7a521ed3078d7ed6ec85cff063ed718ab50
RoziBozi <blazrozman26@gmail.com>
2025-04-03 08:22:33 +0200

    css z ai

--- main.py
+++ main.py
@@ -6,21 +6,33 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template("first_page.html")
+
+
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+
 @app.route("/charts")
 def charts():
     return render_template("charts.html")
+
+
 @app.route("/choice")
 def choice():
     return render_template("choice.html")
+
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
 @app.route("/news")
 def news():
     return render_template("news.html")
+
+
 @app.route("/register")
 def register():
     return render_template("register.html")
