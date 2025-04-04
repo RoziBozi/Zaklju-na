3949049c3091b3cb5db73767c629233d3f3fcfe9
RoziBozi <blazrozman26@gmail.com>
2025-04-04 17:10:42 +0200

    neki kode pa spremembe na html-ju

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, jsonify
-
+import sqlite3
 
 app = Flask(__name__)
+
+conn = sqlite3.connect("main.db")
+
+c = conn.cursor()
+
+c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, gmail TEXT, password TEXT)")
+
+c.execute("SELECT * from users")
+
+conn.commit()
+conn.close()
 
 @app.route("/")
 def main():
@@ -23,8 +34,13 @@ def choice():
     return render_template("choice.html")
 
 
-@app.route("/login")
+@app.route("/login", methods = ["POST","GET"])
 def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password =  request.form["password"]
+    
+        print(username, password)
     return render_template("login.html")
 
 
@@ -33,8 +49,33 @@ def news():
     return render_template("news.html")
 
 
-@app.route("/register")
+@app.route("/register", methods = ["POST","GET"])
 def register():
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password =  request.form["password"]
+        gmail = request.form["gmail"]
+    
+        data = [(username,gmail,password)]
+        conn = sqlite3.connect("main.db")
+        c = conn.cursor()
+        
+        c.execute("SELECT username, gmail FROM users WHERE username = ? OR gmail = ?", (username, gmail))
+
+        for user in c:
+            if username == user[0]:
+                
+                return "Username already taken" 
+            elif gmail == user[1]:
+                
+                return "Gmail already taken"
+
+        c.execute("INSERT INTO users (username, gmail, password) VALUES (?, ?, ?)", (username, gmail, password))  
+
+        conn.commit()
+        conn.close()
+
     return render_template("register.html")
 
 app.run(debug = True)
