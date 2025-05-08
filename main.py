76346fcfd16321d143a22c78a27f6a25d19f9e79
RoziBozi <blazrozman26@gmail.com>
2025-05-08 19:35:01 +0200

    novice delajo

--- main.py
+++ main.py
@@ -82,6 +82,7 @@ def charts():
                             )
         chart = pl.Figure(data=[chart_data])
         chart_za_stran = chart.to_html(full_html=False, include_plotlyjs="cdn")
+
         return jsonify({"chart": chart_za_stran})
 
     return render_template("charts.html")
@@ -117,9 +118,28 @@ def news():
     if request.method == "POST":
         choice = request.form["choice"]
         print(choice)
-        
-        
 
+        news_url_list = ["https://finance.yahoo.com/news/rssindex","https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=15839069","https://feeds.a.dj.com/rss/RSSMarketsMain.xml","https://news.ycombinator.com/rss"]
+        
+                
+        list_artiklov = []
+
+        for news_url in news_url_list:
+            artikel = feedparser.parse(news_url)
+
+            for specificen_artikel in artikel.entries:
+
+                if choice.lower() in specificen_artikel.title.lower():
+                    list_artiklov.append({
+                        "title": specificen_artikel.title,
+                        "link": specificen_artikel.link,
+                        "published": specificen_artikel.published
+                    })
+        print(list_artiklov)
+        if list_artiklov:
+            return jsonify({"article": list_artiklov})
+        else:
+            return jsonify({"article": {}})
 
     return render_template("news.html")
 
